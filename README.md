1ab283d29c6b2d7f1809e88c602cb0e3dc30c5fa
DonnieDice <DonnieDice@ProtonMail.com>
2022-04-14 18:52:43 -0500

    v1.0.0
    Initial Release

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-Remove_Nameplate_Debuffs
+v1.0.0
+ ------------------------------
+
+ Remove Nameplate Debuffs
+
+ ------------------------------
+
+ This is an addon that will remove debuff icons from enemy nameplates.
+
+ ------------------------------
+
+ This addon works in SHADOWLANDS!
+
+ This addon works in CLASSIC WOW!
+
+ This addon works in CLASSIC TBC!
---
+++ main.lua
@@ -0,0 +1,17 @@
+local a = CreateFrame("Frame")
+local events = {}
+
+function events:NAME_PLATE_UNIT_ADDED(plate)
+	local unitId = plate
+	local nameplate = C_NamePlate.GetNamePlateForUnit(unitId)
+	local frame = nameplate.UnitFrame
+	if not nameplate or frame:IsForbidden() then return end
+	frame.BuffFrame:ClearAllPoints()
+	frame.BuffFrame:SetAlpha(0)
+end
+
+for b, u in pairs(events) do
+	a:RegisterEvent(b)
+end
+
+a:SetScript("OnEvent", function(self, event, ...) events[event](self, ...) end)
